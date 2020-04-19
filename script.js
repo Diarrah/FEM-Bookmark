4cdcea1db94f218d9aab86f3e3ffa8d5714121a2
Diarrah <diarrahcasiano@gmail.com>
2020-04-18 18:37:27 -0700

    Mobile nav toggle added

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-// Form validation
-let form = document.querySelector('.form-control'),
-    wrapper = form.querySelector('div'),
-    email = wrapper.querySelector('input'),
-    message = wrapper.querySelector('small');
+//Navbar 
+let mobileNav = document.querySelector('.mobile--nav');
+let burger = document.querySelector('.hamburger');
+let close = document.querySelector('.x');
+let navbar = document.querySelector('.navbar');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let emailVal = email.value.trim();
+burger.addEventListener('click', (e) => {
+    navbar.classList.toggle('hide')
+    mobileNav.classList.toggle('show')
+    close.classList.remove('click')
+});
 
-    if (emailVal === '') {
-        wrapper.classList.add('error')
-        message.innerText = `Email cannot be blank`;
-    } else if (!validateEmail(emailVal)) {
-        wrapper.classList.add('error')
-        message.innerText = `Whoops, make sure it's an email`;
-    } else {
-        wrapper.className = 'form-invalid-wrapper';
-    }
-})
-
-function validateEmail(email) {
-    let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return valid.test(String(email).toLowerCase());
-}
+close.addEventListener('click', (e) => {
+    navbar.classList.toggle('hide')
+    mobileNav.classList.toggle('show')
+    close.classList.add('click')
+});
 
 // Feature Section
 let bookmark = document.getElementById('bookmark');
@@ -65,3 +57,37 @@ share.addEventListener('click', (e) => {
     title.innerText = `Share your bookmarks`
     description.innerText = `Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.`
 })
+
+//Accordion
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', (e) => {
+        question.classList.toggle('faq-active');
+        e.preventDefault();
+    })
+})
+
+// Form validation
+let form = document.querySelector('.form-control'),
+    wrapper = form.querySelector('div'),
+    email = wrapper.querySelector('input'),
+    message = wrapper.querySelector('small');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let emailVal = email.value.trim();
+
+    if (emailVal === '') {
+        wrapper.classList.add('error')
+        message.innerText = `Email cannot be blank`;
+    } else if (!validateEmail(emailVal)) {
+        wrapper.classList.add('error')
+        message.innerText = `Whoops, make sure it's an email`;
+    } else {
+        wrapper.className = 'form-invalid-wrapper';
+    }
+})
+
+function validateEmail(email) {
+    let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return valid.test(String(email).toLowerCase());
+}
